c374c0af8f94a4f0a3e6c15c91c5b4c01ea271ad
Hoshi Holiday <hoshiholiday@gmail.com>
2022-09-14 07:37:40 -0500

    Update Navbar.jsx
    coded the suggested update for the badge from the console.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         <div className={classes.grow}/>
         <div className={classes.button}>
             <IconButton aria-label="Show cart items" color="inherit"></IconButton>
-            <Badge badgeContent={2} color="secondary">
+            <Badge badgeContent={2} color="secondary" overlap="rectangular">
                 <ShoppingCart/>
             </Badge> 
         </div>
