ae0c102362c47955e26acd1f75fa767fe5a72fc6
Hoshi Holiday <hoshiholiday@gmail.com>
2022-11-16 05:55:50 -0600

    Research
    researched related problems on github to see if the fields on custom textfield and its required property could be triggering my error. it was not. at this point I am researching gateway and api configuration.

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -3,18 +3,21 @@ import { TextField,Grid } from '@material-ui/core';
 import {useFormContext,Controller} from 'react-hook-form';
 
 
-const FormInput = ({name,label}) => {
+const FormInput = ({name,label,required}) => {
     const {control} = useFormContext();
+    const isError = false;
+
   return (
     <Grid item xs={12} sm={6}>
      <Controller
         as={TextField}
-        defaultValue=""
-        control={control}
-        fullWidth
         name={name}
+        control={control}
         label={label}
-        required/>
+        fullWidth
+        required={required}
+        error={isError}
+        />
     </Grid>
   );
 }
